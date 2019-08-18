9bbba9c020fb7db20ada5d5b42cd772d72b9b94d
Pablo <pablo.albendea@outlook.com>
2019-08-18 22:07:29 +0200

    added token und basic map setup

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGFibG9hbGJpYW4iLCJhIjoiY2p6Z3kxbDh1MGxlMzNob29yaDlqNnlrcCJ9.PkehJd3-j7LfVWsjkk8OzQ';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-74.50, 40],
+  zoom: 9
+});
