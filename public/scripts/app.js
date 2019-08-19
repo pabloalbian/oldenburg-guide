83025afb280f4c6f29afcfe594a6af62d782fe86
Pablo <pablo.albendea@outlook.com>
2019-08-19 23:26:09 +0200

    code reorganisation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,8 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFibG9hbGJpYW4iLCJhIjoiY2p6Z3kxbDh1MGxlMzNob29yaDlqNnlrcCJ9.PkehJd3-j7LfVWsjkk8OzQ';
 
 var map = new mapboxgl.Map({
-	container: 'map', // container id
-	style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-	center: [8.213068, 53.140160], // starting position [lng, lat]
-	zoom: 13
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+  center: [8.213068, 53.140160], // starting position [lng, lat]
+  zoom: 13
 });
--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -1,14 +1,14 @@
-$("#showbtn").click(function(){
-	$(this).removeClass("showbtn-left").addClass("showbtn-right");
+$("#showbtn").click(function() {
+  $(this).removeClass("showbtn-left").addClass("showbtn-right");
 
-	window.setTimeout(function(){
-		$("#showbtn").addClass("showbtn-hide");
-	}, 500);
+  window.setTimeout(function() {
+    $("#showbtn").addClass("showbtn-hide");
+  }, 500);
 
-	$('#mySidebar').css("width", "250px");
+  $('#mySidebar').css("width", "250px");
 });
 
-$(".closebtn").click(function(){
-	$("#showbtn").removeClass("showbtn-hide showbtn-right").addClass("showbtn-left");
-	$("#mySidebar").css("width", "0px");
+$(".closebtn").click(function() {
+  $("#showbtn").removeClass("showbtn-hide showbtn-right").addClass("showbtn-left");
+  $("#mySidebar").css("width", "0px");
 });
