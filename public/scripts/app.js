3fa0f9de3d398303edebca850877d0198f36ccc7
Pablo <pablo.albendea@outlook.com>
2019-08-21 21:45:07 +0200

    Added function to app.js that creates list of locations on sidebar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -109,17 +109,46 @@ var locData = {
   ]
 }
 
-//MAP CREATION
+// TOKEN AND CREATING MAP OBJECT
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFibG9hbGJpYW4iLCJhIjoiY2p6Z3kxbDh1MGxlMzNob29yaDlqNnlrcCJ9.PkehJd3-j7LfVWsjkk8OzQ';
 
 var map = new mapboxgl.Map({
-  container: 'map', // container id
+  container: 'map',
   style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-  center: [8.213068, 53.140160], // starting position [lng, lat]
+  center: [8.213068, 53.140160], // starting coordinates
   zoom: 13
 });
 
+// SHOW OUR LOCATIONS LIST ON THE SIDEBAR
+
+function buildLocationList(data) {
+  // Iterating through the locations list
+  for (i = 0; i < data.features.length; i++) {
+    var currentFeature = data.features[i];
+    // 'prop' will store the properties of our current location element
+    var prop = currentFeature.properties;
+    // Selecting the list in the sidebar and create a div for each location
+    var list = document.getElementById('list');
+    var listing = list.appendChild(document.createElement('div'));
+    listing.className = 'location';
+    listing.id = 'location-' + i;
+
+    // Creating a link for each location with the name as text
+    var link = listing.appendChild(document.createElement('h2'));
+    // link.href = '#'; use this to create the link later, changing h2 to a
+    link.className = 'name';
+    link.dataPosition = i;
+    link.innerHTML = prop.name;
+
+    // Creating a div for each location's address
+    var address = listing.appendChild(document.createElement('div'));
+    address.className = 'address'
+    address.innerHTML = prop.address;
+  }
+}
+
+// ADD LOCATIONS TO MAP AND CREATE LIST ON SIDEBAR
 
 map.on('load', function(e) {
   map.addLayer({
@@ -134,4 +163,5 @@ map.on('load', function(e) {
       'icon-allow-overlap': true,
     }
   });
+  buildLocationList(locData);
 });
